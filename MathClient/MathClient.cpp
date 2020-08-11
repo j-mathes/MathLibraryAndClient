024feb98a12a963e9ac1588b42c845b6774d29fb
Jared Mathes <j-mathes@users.noreply.github.com>
2020-08-10 18:01:19 -0600

    Add project files.

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,25 @@
+// MathClient.cpp : Defines the entry point for the console application.  
+// Compile by using: cl /EHsc /link MathLibrary.lib MathClient.cpp  
+
+#include "stdafx.h"  
+#include <iostream>  
+#include "MathLibrary.h"  
+
+using namespace std;
+
+int main()
+{
+	double a = 7.4;
+	int b = 99;
+
+	cout << "a + b = " <<
+		MathLibrary::Functions::Add(a, b) << endl;
+	cout << "a * b = " <<
+		MathLibrary::Functions::Multiply(a, b) << endl;
+	cout << "a + (a * b) = " <<
+		MathLibrary::Functions::AddMultiply(a, b) << endl;
+
+	return 0;
+}
+
+
